0f0bfc4aff68bedb827bc0ef134103d8c9422c64
Istiak Alam <168295675+Istiaq-Alam@users.noreply.github.com>
2026-03-06 01:52:06 +0600

    web

---
+++ WebDoc/script.js
@@ -0,0 +1,21 @@
+// simple scroll animation
+
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+
+cards.forEach(card => {
+
+const position = card.getBoundingClientRect().top;
+const screen = window.innerHeight;
+
+if(position < screen - 100){
+
+card.style.opacity = "1";
+card.style.transform = "translateY(0px)";
+
+}
+
+});
+
+});
